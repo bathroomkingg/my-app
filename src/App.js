0c7938edc2b1c516abde9481166f43a007e4a4b2
Xiaorong Liang <xiaorol@uci.edu>
2024-05-26 12:56:39 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-import { NavBar } from './Componets/NavBar.JS';
-import 'bootstrap/dis/css/bootstrap.min.css';
+import { NavBar } from "./components/NavBar";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
